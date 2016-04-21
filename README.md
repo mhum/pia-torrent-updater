790545ad152b1b96e66b87b809975f5f14d5c50c
Michael <Michael@MichaelHumiston.com>
2016-04-21 22:59:17 +1000

    Add debug mode for troubleshooting

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Private Internet Access Transmission Updater
-This script will update the listening port for [Transmission](http://www.transmissionbt.com) 
+This script will update the listening port for [Transmission](http://www.transmissionbt.com)
 with the forwarding port for users of [Private Internet Access](https://www.privateinternetaccess.com).
 
 ## How It Works
 There are three steps to this script. First, it makes a remote procedure call to Transmision to see if
-the current listening port is still open. If it isn't, the script gathers the local ip address for the 
+the current listening port is still open. If it isn't, the script gathers the local ip address for the
 VPN device, and makes a request to PIA with your user name, password, and client id for the current
 forwarding port. Finally, another remote procedure call is made to Transmission to update the listening
 port with the forwarding port sent back by PIA.
@@ -16,7 +16,7 @@ pre-installed on most *nix operating systems.
 ## Configuration
 All configurations are made at the very top of the file in the `CONFIG` section.
 ```tcl
-set user      USERNAME  
+set user      USERNAME
 set pass      PASSWORD
 set id_file   CLIENT_ID_FILE
 set pia_url   http://www.privateinternetaccess.com/vpninfo/port_forward_assignment
@@ -24,23 +24,26 @@ set trans_url TRANSMISSION_URL/transmission/rpc
 set device    VPN_DEVICE
 ```
 ```
-USERNAME         ---PIA user name  
-PASSWORD         ---PIA password  
-CLIENT_ID_FILE   ---Path to file containing client id  
-TRANSMISSION_URL ---URL to transmission. If running on same box: http://localhost:9091  
+USERNAME         ---PIA user name
+PASSWORD         ---PIA password
+CLIENT_ID_FILE   ---Path to file containing client id
+TRANSMISSION_URL ---URL to transmission. If running on same box: http://localhost:9091
 VPN_DEVICE       ---Device name for VPN connection. Usually tun0. Run ifconfig to find out
 ```
 
 ## Running It
 First, we need a file with the unique, constant client id. This will be sent with every request.
-> **OS X**:   head -n 100 /dev/urandom | md5 > ~/.pia_client_id  
-> **Linux**: head -n 100 /dev/urandom | md5sum | tr -d " -" > ~/.pia_client_id 
+> **OS X**:   head -n 100 /dev/urandom | md5 > ~/.pia_client_id
+> **Linux**: head -n 100 /dev/urandom | md5sum | tr -d " -" > ~/.pia_client_id
 
-Once the client id file is created and the configurations set, it is as easy as running:  
-`tclsh updater.tcl`  
-or make it executable with `chmod u+x updater.tcl` and then run  
+Once the client id file is created and the configurations set, it is as easy as running:
+`tclsh updater.tcl`
+or make it executable with `chmod u+x updater.tcl` and then run
 `./updater.tcl`
 
+It can also be ran in a debugging mode to troubleshoot issues by adding a flag when running:
+`./updater.tcl --debug true`
+
 ## Scheduling It
-It can even be setup to run as a cron job to completely automate this process. Something such as:  
+It can even be setup to run as a cron job to completely automate this process. Something such as:
 > @hourly /usr/local/bin/tclsh /scripts/updater.tcl
--- updater.tcl
+++ updater.tcl
@@ -12,55 +12,147 @@ set device    VPN_DEVICE
 package require http
 package require json
 
+#------------------------------Setup Command Line Configs--------------------------#
+set CFG(configs) [list debug]
+set CFG(debug)   false
+
+if {[expr {$argc % 2}] != 0} {
+	puts "Invalid configs. Must provide config and value. Allowed configs are: $CFG(configs)"
+	exit
+}
+
+foreach {arg value} $argv {
+	set argz [string range $arg 2 end]
+
+	if {[lsearch $CFG(configs) $argz] < 0} {
+		puts "Invalid config. Allowed configs are: $CFG(configs)"
+		exit
+	}
+
+	set CFG($argz) $value
+}
+
 #------------------------------Check Transmission----------------------------------#
 #Fetch CSRF token first
+if {$CFG(debug)} {
+	puts "DEBUG: Connecting to transmission at: $trans_url"
+}
+
 set resp_token [http::geturl $trans_url]
 set response [http::meta $resp_token]
+
+if {$CFG(debug)} {
+	puts "DEBUG: Response received:"
+	puts "DEBUG: $response"
+}
+
 http::cleanup $resp_token
 
 set csrf [lindex $response 3]
 
 if {$csrf eq {}} {
-        puts "Error: Unable to retrieve CSRF token"
-        puts "Response Received: $response"
-        return
+	puts "Error: Unable to retrieve CSRF token"
+	puts "Response Received: $response"
+	return
+}
+
+if {$CFG(debug)} {
+	puts "DEBUG: Retrieved CSRF token: $csrf"
 }
 
 #Check if port is still open
 set query {{"arguments": {},"method": "port-test"}}
 set headers "X-Transmission-Session-Id $csrf"
+
+if {$CFG(debug)} {
+	puts "DEBUG: Send query: $query"
+	puts "DEBUG: With headers: $headers"
+	puts "DEBUG: To $trans_url"
+}
+
 set resp_token [http::geturl $trans_url -query $query -headers $headers]
 set resp_data [json::json2dict [http::data $resp_token]]
+http::cleanup $resp_token
+
+if {$CFG(debug)} {
+	puts "DEBUG: Received: $resp_data"
+}
 
 #Exit if port is still open
 set port_open [dict get [dict get $resp_data arguments] port-is-open]
+
+if {$CFG(debug)} {
+	puts "DEBUG: Port open: $port_open"
+}
+
 if {$port_open} {
 	puts "Port is still open"
 	return
 }
 puts "Port is no longer open. Let's continue!"
-http::cleanup $resp_token
 
 #------------------------------Fetch Port------------------------------------------#
 #Pull client id out of file
+if {$CFG(debug)} {
+	puts "DEBUG: Read client id from: $id_file"
+}
+
 set fp [open $id_file r]
 set file_data [read $fp]
 close $fp
 set client_id $file_data
 
+if {$CFG(debug)} {
+	puts "DEBUG: Client id: $client_id"
+}
+
 #Make request
+if {$CFG(debug)} {
+	puts "DEBUG: Run ifconfig for $device:"
+	puts [exec ifconfig $device]
+}
 regexp {inet .*?(10\.[0-9]+\.[0-9]+\.[0-9]+)} [exec ifconfig $device] -> local_ip
+
+if {$CFG(debug)} {
+	puts "DEBUG: Found IP: $local_ip"
+}
+
 set query [::http::formatQuery user $user pass $pass client_id $client_id \
 		   local_ip $local_ip]
+
+if {$CFG(debug)} {
+	puts "DEBUG: Format query to fetch port: $query"
+	puts "DEBUG: Sending to: $pia_url"
+}
+
 set resp [json::json2dict [http::data [http::geturl $pia_url -query $query]]]
+
+if {$CFG(debug)} {
+	puts "DEBUG: Response:"
+	puts "DEBUG: $response"
+}
+
 set port [dict get $resp port]
 puts "Fetched new port: $port"
 
 #------------------------------Update Transmission---------------------------------#
 set query "{\"arguments\": {\"peer-port\":$port},\"method\": \"session-set\"}"
 set headers "X-Transmission-Session-Id $csrf"
+
+if {$CFG(debug)} {
+	puts "DEBUG: Send request to Transmission"
+	puts "DEBUG: Query: $query"
+	puts "DEBUG: Headers: $headers"
+}
+
 set resp_token [http::geturl $trans_url -query $query  -headers $headers]
 set resp_data [json::json2dict [http::data $resp_token]]
+
+if {$CFG(debug)} {
+	puts "DEBUG: Response:"
+	puts "DEBUG: $resp_data"
+}
+
 if {[dict get $resp_data result] eq {success}} {
 	puts "Port successfully updated"
 } else {
